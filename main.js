ca8cc62fc8a96fe51bc0be197f497b2cead357af
HaiCDT <henryhaivt@gmail.com>
2024-12-25 21:48:26 +0700

    Update main.js
    cập nhật code lần cuối

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ import { engine } from 'express-handlebars';
 import numeral from 'numeral';
 import session from 'express-session';
 import path from 'path';
+import bodyParser from 'body-parser';
 const app = express();
 
+// Middleware để phân tích cú pháp JSON
+app.use(bodyParser.json());
+
+// Middleware để phân tích cú pháp dữ liệu URL-encoded
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
   secret: 'SECRET_KEY',
@@ -33,6 +39,8 @@ app.use('/uploads', express.static('uploads'));
 app.use('/articles', articlesRouter);
 app.get('/', (req, res) => res.redirect('/articles/home'));
 import articlesRouter from './routes/articles.route.js';
+app.use('/account', accountRouter);
+import accountRouter from './routes/account.route.js';
 app.listen(3000, function () {
   console.log('Server started on http://localhost:3000');
 });
